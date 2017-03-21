ed5038a4d9ef1da8caab1f90c38c8127a84c0406
Jibran Kalia <jkalia@e1z2r9p33.42.us.org>
2017-03-21 16:47:46 -0700

    Almost there

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jkalia <jkalia@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/12 19:46:18 by jkalia            #+#    #+#             */
-/*   Updated: 2017/03/20 18:10:39 by jkalia           ###   ########.fr       */
+/*   Updated: 2017/03/21 16:45:38 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,40 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void	insert_extra(char *buf, char **extra, size_t size)
+void	ft_join_free(char *buf, char **extra)
 {
-	size_t	original_len;
+	char	*tmp;
 
-	original_len = size;
-	if (!*extra)
+	tmp = ft_strjoin(*extra, buf);
+	free(*extra);
+	ft_bzero(buf, BUFF_SIZE + 1);
+	*extra = tmp;
+}
+
+void	insert_extra(char *buf, char **extra)
+{
+	if (*extra == 0)
 	{
 		*extra = ft_strdup(buf);
+		ft_bzero(buf, BUFF_SIZE + 1);
 		return ;
 	}
-	*extra = ft_strjoin(*extra, buf);
+	ft_join_free(buf, extra);
 }
 
 int		assign_line(char **extra, char **line, size_t size)
 {
-	if (**extra == '\n')
-		size++;
 	*line = ft_strndup(*extra, size);
-	ft_memmove(*extra, *extra + size + 1, size);
+	*extra = ft_memmove(*extra, *extra + size + 1, ft_strlen(*extra));
 	return (1);
 }
 
 int		final_line(char **extra, char **line)
 {
-	size_t	extra_len;
+	size_t extra_len;
 
-	printf("Extra2 = %s\n", *extra);
 	extra_len = ft_strlen(*extra);
-	*line = ft_strndup(*extra, extra_len);
+	*line = ft_strndup(*extra, ft_strlen(*extra));
 	ft_bzero(*extra, extra_len);
 	return (1);
 }
@@ -56,17 +61,22 @@ int		get_next_line(const int fd, char **line)
 
 	if (fd < 0 || !line || BUFF_SIZE == 0 || fd > GNL_MAX_FD)
 		return (-1);
+	ft_bzero(buf, BUFF_SIZE + 1);
 	while ((b_read = read(fd, buf, BUFF_SIZE)) != 0)
 	{
 		CHK(b_read == -1, -1);
-		insert_extra(buf, &extra[fd], b_read);
+		insert_extra(buf, &extra[fd]);
 		if ((tmp = ft_strchr(extra[fd], '\n')) != 0)
 			return (assign_line(&extra[fd], line, tmp - extra[fd]));
 	}
 	if ((tmp = ft_strchr(extra[fd], '\n')) != 0)
+	{
 		return (assign_line(&extra[fd], line, tmp - extra[fd]));
-	else if (ft_strlen(extra[fd]))
+	}
+	else if (ft_strlen(extra[fd]) > 0)
+	{
 		return (final_line(&extra[fd], line));
-	free(extra[fd]);
+	}
+	//free(extra[fd]);
 	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jkalia <jkalia@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/12 19:46:41 by jkalia            #+#    #+#             */
-/*   Updated: 2017/03/20 15:56:43 by jkalia           ###   ########.fr       */
+/*   Updated: 2017/03/21 16:47:11 by jkalia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@
 # include <sys/uio.h>
 # include <unistd.h>
 # include <stdlib.h>
-# include "libft/libft.h"
+# include <libft.h>
 
-# define BUFF_SIZE 5
+# define BUFF_SIZE 1
 # define GNL_MAX_FD 4864
 
 typedef	enum	e_bool
