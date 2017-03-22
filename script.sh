13b968d431503dfd5ea583bf9e2af0d1d4fd6438
Jibran Kalia <jkalia@e1z2r7p10.42.us.org>
2017-03-22 16:27:56 -0700

    Submitted

--- script.sh
+++ script.sh
@@ -3,6 +3,10 @@ CFLAGS="-Wall -Wextra -Werror -I libft/includes"
 clang $CFLAGS -o get_next_line.o -c get_next_line.c
 clang -Wall -Wextra -Werror -I libft/includes -o main.o -c main.c
 clang -Wall -Wextra -Werror -I libft/includes -o alloc_wrap.o -c alloc_wrap.c
+
+# With Alloc Test
 clang -o test_gnl alloc_wrap.o main.o get_next_line.o -I libft/includes -L libft/ -lft
+
+# Without Alloc Test
 #clang -o test_gnl main.o get_next_line.o -I libft/includes -L libft/ -lft
 rm *.o
